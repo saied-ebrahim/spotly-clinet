02e58b69c6fbee3c5ff64f076d2744972b0e44ca
tareq <tareq@mail.com>
2025-12-04 15:10:46 +0200

    added tecket select modal & husain Map component

--- src/components/ui/details/TicketSidebar.tsx
+++ src/components/ui/details/TicketSidebar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { EventObject } from "@/types/PaginationInterface";
 import { formatPrice } from "@/utils/details/formatting";
 import { FaTicketAlt } from "react-icons/fa";
+import { useState } from "react";
+import TicketsModal from "@/components/Custom/TicketsModal";
+import { EventDocument } from "@/types/eventInterface";
 
-export default function TicketSidebar({ event }: { event: EventObject }) {
+export default function TicketSidebar({ event }: { event: EventDocument }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="lg:col-span-1">
       <div className="sticky top-8 space-y-6">
@@ -14,7 +19,7 @@ export default function TicketSidebar({ event }: { event: EventObject }) {
             <div className="flex items-center gap-3 text-gray-700 mb-2">
               <FaTicketAlt className="text-gray-400 rotate-90" />
               <span className="font-medium text-sm">
-                Standard Ticket: {formatPrice(event.price)}
+                Standard Ticket: {formatPrice(event.ticketType.price)}
               </span>
             </div>
             <div className="text-xs text-gray-500 mt-2">
@@ -26,12 +31,18 @@ export default function TicketSidebar({ event }: { event: EventObject }) {
             </div>
           </div>
 
-          <button className="w-full py-3.5 bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-bold rounded-xl shadow-sm hover:shadow-md transition-all active:scale-[0.98] flex justify-center items-center gap-2">
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="w-full py-3.5 bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-bold rounded-xl shadow-sm hover:shadow-md transition-all active:scale-[0.98] flex justify-center items-center gap-2"
+          >
             <FaTicketAlt className="-rotate-45" />
-            {parseInt(event.price as string) > 0
-              ? "Buy Tickets"
-              : "Get Tickets"}
+            {event.ticketType.price > 0 ? "Buy Tickets" : "Get Tickets"}
           </button>
+          <TicketsModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            event={event}
+          />
         </div>
       </div>
     </div>
