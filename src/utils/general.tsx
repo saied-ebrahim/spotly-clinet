96d175f5ed2e9d7959a5ba61191eb7f725c6b36d
tareq <tareq@mail.com>
2025-12-05 17:45:02 +0200

    modified code

--- src/utils/general.tsx
+++ src/utils/general.tsx
@@ -1,5 +1,5 @@
 export const getImageUrl = (url?: string) => {
-  if (!url) return "/events.json";
+  if (!url) return "/event.jpg";
   if (url.startsWith("http") || url.startsWith("/")) return url;
   return `https://${url}`;
 };
